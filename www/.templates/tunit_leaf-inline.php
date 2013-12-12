6cb30b5dadc01665bba317e555909411ed1002c6
NikolayRag <nikolayr@mail.ru>
2013-12-12 19:30:39 +0300

    El-inline: replace Hilite/Shadow with css

--- www/.templates/tunit_leaf-inline.php
+++ www/.templates/tunit_leaf-inline.php
@@ -1,8 +1,6 @@
 
 <?=$NOPROFILE? '': "<!-- in template LeafInline -->\n"?>
 <div id='elInlineTmpl' class='elInline'>
-	<div id='elInlineHil' class='elInlineHil'></div>
-	<div id='elInlineSh' class='elInlineSh'></div>
 	<div id='elInlineTxt' class='elInlineTxt'></div>
 </div>
 <?=$NOPROFILE? '': "<!-- out template LeafInline -->\n"?>
